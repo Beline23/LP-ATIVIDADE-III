87feeca00e9cadf8a2dcc50c09c7a3b75194aba3
Beline23 <144127688+Beline23@users.noreply.github.com>
2023-11-28 12:11:49 -0300

    Atividade 05.c

--- Atividade 05.c
+++ Atividade 05.c
@@ -18,14 +18,14 @@ int main()
 
     int opcao;
     int i;
-    int contador = 0;
-    char nomeProduto[999][200];
-    int quantidade[200];
-    float valorProduto[200];
+    int X = 0;
+    char Produto[999][200];
+    int QT [200];
+    float VProduto[200];
 
     do
     {
-        printf("||| MENU DE VENDAS ||| \n");
+        printf(" ||| MENU DE VENDAS ||| \n");
         printf("\n");
         printf("\n");
         printf("1) Adicionar venda \n");
@@ -37,37 +37,37 @@ int main()
 
         fflush(stdin);
 
-        system("cls || clear");
-
+       
         switch (opcao)
         {
         case 1:
 
-            printf("Digite  o nome do produto: ");
-            gets(nomeProduto[contador]);
+            printf("Nome do Produto: ");
+            gets(Produto[X]);
 
             fflush(stdin);
 
             printf("Digite o valor do produto: ");
-            scanf("%f", &valorProduto[contador]);
+            scanf("%f", &VProduto[X]);
 
             printf("Digite a quantidade do produto: ");
-            scanf("%d", &quantidade[contador]);
+            scanf("%d", &QT[X]);
 
-            contador++;
-
-            system("cls || clear");
-
+            X++;
+            
             break;
         case 2:
 
-            for (i = 0; i < contador; i++)
+            for (i = 0; i < X; i++)
             {
 
                 printf("%dº vendas\n", i + 1);
-                printf("Produtos: %s\n", nomeProduto[i]);
-                printf("Valor dos produtos: %.2f\n", valorProduto[i]);
-                printf("Quantidade: %d\n", quantidade[i]);
+                printf("\n");
+                printf("Produtos: %s\n", Produto[i]);
+                printf("\n");
+                printf("Valor dos produtos: %.2f\n", VProduto[i]);
+                printf("\n");
+                printf("Quantidade: %d\n", QT[i]);
                 printf("\n");
             }
             break;
